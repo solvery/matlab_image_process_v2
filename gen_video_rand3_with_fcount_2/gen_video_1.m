907ca8b2d67ac53f1eb947bdd50b132bcc49f94b
solvery@163.com <solvery@163.com>
2019-05-21 18:51:29 +0800

    /home/yang/workspace/matlab_image_process_v2/gen_video_rand3_with_fcount_2 at gen_video_rand3_with_fcount_2

--- gen_video_rand3_with_fcount_2/gen_video_1.m
+++ gen_video_rand3_with_fcount_2/gen_video_1.m
@@ -2,7 +2,7 @@
 function gen_video_1(step, frame_rate)
 
 img_1 = rand(1080, 1920, 3);
-filename='video_rand3_with_fcount_1';
+filename='video_rand3_slide_with_fcount_1';
 
 fn_out = [filename '_1080_f' num2str(frame_rate,'%02d') ];
 video_1 = VideoWriter(fn_out);
@@ -21,7 +21,33 @@ for pos = 0:step:6000
     for ii=1:4
         text_str{ii} = ['F' num2str(ii) '-' num2str(conf_val(ii),'%04d') ' '];
     end
-
     img_2 = insertText(img_1,position, text_str, 'FontSize', 100, 'BoxColor', box_color,'TextColor','white');
+
     writeVideo(video_1, img_2);
 end
+
+for pos = step:step:1920
+    img_2 = img_1;
+    img_2(:,1:pos,:) = 0;
+    conf_val = [pos pos pos pos];
+    for ii=1:4
+        text_str{ii} = ['F' num2str(ii) '-' num2str(conf_val(ii),'%04d') ' '];
+    end
+    img_3 = insertText(img_2,position, text_str, 'FontSize', 100, 'BoxColor', box_color,'TextColor','white');
+
+
+    writeVideo(video_1, img_3);
+end
+for pos = 1920:-step:step
+    img_2 = img_1;
+    img_2(:,1:pos,:) = 0;
+    conf_val = [pos pos pos pos];
+    for ii=1:4
+        text_str{ii} = ['F' num2str(ii) '-' num2str(conf_val(ii),'%04d') ' '];
+    end
+    img_3 = insertText(img_2,position, text_str, 'FontSize', 100, 'BoxColor', box_color,'TextColor','white');
+
+
+    writeVideo(video_1, img_3);
+end
+
