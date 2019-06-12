983f6cb2b16b59351c81ad2cb0efeac3868d0ba7
solvery@163.com <solvery@163.com>
2019-06-12 11:01:45 +0800

    1 image2video_video2image_1

---
+++ image2video_video2image_1/gen_video_image_1.m
@@ -0,0 +1,35 @@
+
+%function gen_video_1(img_fn, vfn_prefix, step, frame_rate)
+
+%img_1 = imread(img_fn);
+%img_1 = imread('excel-detail-1080p.png');
+img_1 = rand(1080, 1920, 3);
+filename='rand3_fcount_';
+frame_rate=60
+step=1
+
+fn_out = [filename '_1080_f' num2str(frame_rate,'%02d') ]
+video_1 = VideoWriter(fn_out,'Uncompressed AVI');
+video_1.FrameRate = frame_rate;
+open(video_1);
+
+
+text_str = cell(16,1);
+position = [ 192 0; 192 135*1; 192 135*2; 192 135*3; 192 135*4; 192 135*5; 192 135*6; 192 135*7; 1152 0; 1152 135*1; 1152 135*2; 1152 135*3; 1152 135*4; 1152 135*5; 1152 135*6; 1152 135*7 ];
+box_color = { 'black', 'black','black', 'black', 'black', 'black','black', 'black', 'black', 'black','black', 'black', 'black', 'black','black', 'black' };
+
+
+for pos = 0:step:10
+    conf_val = [pos pos pos pos pos pos pos pos pos pos pos pos pos pos pos pos];
+    for ii=1:16
+        text_str{ii} = ['F' num2str(ii, '%02d') '-' num2str(conf_val(ii),'%04d') ' '];
+    end
+
+    img_2 = insertText(img_1,position, text_str, 'FontSize',80,'BoxColor', box_color,'TextColor','white');
+    fn_out = ['img_orig/img_rand3_fcount_' num2str(pos,'%04d') '.bmp'];
+    imwrite(img_2, fn_out);
+    writeVideo(video_1, img_2);
+
+end
+
+close(video_1)
