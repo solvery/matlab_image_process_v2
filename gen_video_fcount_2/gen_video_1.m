215f63e91990a3fec1547d52f113ebbc2daa6b64
solvery@163.com <solvery@163.com>
2019-07-04 09:50:36 +0800

    1 gen_video_fcount_2

--- gen_video_fcount_2/gen_video_1.m
+++ gen_video_fcount_2/gen_video_1.m
@@ -3,10 +3,8 @@ function gen_video_1(img_in, vfn_prefix, step, frame_rate)
 
 % gen_video_1('mc_blue.png', 'video_mc_blue', 1, 60);
 
-img_1 = rand(100, 200, 3);
-img_1(:,:,:) = 0;
-%img_1 = imread('excel-detail-1080p.png');
-%img_1 = rand(1080, 1920, 3);
+img_1 = zeros(100, 150, 3);
+
 filename=vfn_prefix;
 
 fn_out = [filename num2str(frame_rate,'%02d') ];
@@ -19,12 +17,10 @@ position = [0 0];
 box_color = { 'black' };
 ii = 1;
 
-for pos = 0:step:3000
-    conf_val = [mod(pos, frame_rate)];
-    
-    text_str{ii} = [ num2str(pos,'%04d') '-' num2str(conf_val,'%02d') ' '];
-
-    img_2 = insertText(img_1,position, text_str, 'FontSize',40,'BoxColor', box_color,'BoxOpacity',1,'TextColor','white');
-    writeVideo(video_1, img_2);
-
+for ix = 0:1:99
+    for iy = 1:1:frame_rate
+        text_str{ii} = [ num2str(ix,'%02d') '-' num2str(iy,'%02d') ];
+        img_2 = insertText(img_1,position, text_str, 'FontSize',40,'BoxColor', box_color,'BoxOpacity',1,'TextColor','white');
+        writeVideo(video_1, img_2);
+    end
 end
