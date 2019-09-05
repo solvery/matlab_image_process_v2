41a4447c95fed7f0a8e33ec402d054f41a29b376
solvery@163.com <solvery@163.com>
2019-09-05 09:27:45 +0800

    1 matlab_image_process_v2

---
+++ cap_diff_1/run_1.m
@@ -0,0 +1,20 @@
+% img_comp_4k_1('cap_nvx_loopback_1');
+% img_comp_4k_1('cap_nvx_500m');
+% img_comp_4k_1('cap_nvx_700m');
+% img_comp_4k_1('cap_nvx_900m');
+% img_comp_4k_1('cap_nvx_950m');
+% img_comp_4k_1('cap_aspeed_4k_1');
+% img_comp_4k_1('cap_zcu106_sub_frame_4k_1');
+% img_comp_4k_1('cap_zcu106_normal_4k_1');
+% img_comp_4k_1('cap_凯撒中文4k30_1');
+% img_comp_4k_1('cap_pc_1');
+% img_comp_4k_1('cap_hdcp_remove_1');
+% img_comp_4k_1('cap_4k_pc_hdcp_remove_1');
+img_comp_4k_1('cap_vishare_fpga_4k_1');
+
+
+% img_comp_2k_1('cap_aspeed_2k_1');
+% img_comp_2k_1('cap_凯撒中文4k30_1080p_1');
+% img_comp_2k_1('cap_凯撒中文2k_1');
+% img_comp_2k_1('cap_ihse2k_1');
+
