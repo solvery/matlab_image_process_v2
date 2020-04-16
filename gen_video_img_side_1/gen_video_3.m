02983dcae92b72cf81f5dc61c93fc0d0ca160887
solvery@163.com <solvery@163.com>
2020-04-16 08:33:57 +0800

    1 matlab_image_process_v2

--- gen_video_img_side_1/gen_video_3.m
+++ gen_video_img_side_1/gen_video_3.m
@@ -1,22 +1,20 @@
 
 function gen_video_3(step, frame_rate)
 
-img_1 = rand(1080, 1920, 3);
-filename='video_rand3_slide_right_1';
+img_1 = imread('..\img_data_1\img (1).jpg');
+img_2 = imread('..\img_data_1\img (2).jpg');
+img_3 = imread('..\img_data_1\img (3).jpg');
+img_t1 = [img_1, img_2, img_3];
+
+filename='video_img_slide_1';
 
 fn_out = [filename '_1080_f' num2str(frame_rate,'%02d') ];
 video_1 = VideoWriter(fn_out);
 video_1.FrameRate = frame_rate;
 open(video_1);
 
-for pos = step:step:1920
-    img_2 = img_1;
-    img_2(:,1:pos,:) = 0;
-    writeVideo(video_1, img_2);
-end
-for pos = 1920:-step:step
-    img_2 = img_1;
-    img_2(:,1:pos,:) = 0;
-    writeVideo(video_1, img_2);
+for pos = 1:1:1920*2
+    img_t2 = img_t1(:,pos:pos+1920,:);
+    writeVideo(video_1, img_t2);
 end
 
